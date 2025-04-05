2c32be6762e8c4d6eaed144052574af7837fe93e
João Miccuci <joaomicucci3@gmail.com>
2025-04-05 01:18:30 -0300

    parte 1 logica de funcionamento do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
+
 
 @Component({
   selector: 'app-quizz',
@@ -29,7 +31,37 @@ export class QuizzComponent implements OnInit {
   
   
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+   
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      
+    }
   }
 
   // !  //
