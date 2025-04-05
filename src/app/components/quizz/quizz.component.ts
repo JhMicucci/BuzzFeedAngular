20fbcf30eddf2fda3f3ef5938094024181cffc03
João Miccuci <joaomicucci3@gmail.com>
2025-04-05 00:37:03 -0300

    first commit

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  imports: [],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+
+  // !  //
+
+}
